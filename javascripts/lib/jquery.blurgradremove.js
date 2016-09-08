72b8e09c2e3783ca34c4e9c9da97c01344cd051d
jun takeno <take.yapr@gmail.com>
2016-09-08 18:57:17 +0900

    ファイル作成

---
+++ javascripts/lib/jquery.blurgradremove.js
@@ -0,0 +1,18 @@
+
+(function( $ ) {
+  'use strict';
+
+  $.fn.blurgradremove = function( options ) {
+    console.log(this);
+
+    return this.each(function() {
+
+    });
+
+  };
+})( jQuery );
+
+
+//if(!$('#' + custom_class, ifrmDoc).length){
+//  $('body', ifrmDoc).append('<style id=\'' + custom_class + '\'></style>');
+//}
